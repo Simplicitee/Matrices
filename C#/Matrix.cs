235ea4785e81b80b1e4847c749aa66b63ac55dbf
Benford Whitaker <simplicitee123@gmail.com>
2020-02-03 12:48:26 -0500

    Update Matrix.cs

--- C#/Matrix.cs
+++ C#/Matrix.cs
@@ -1,112 +1,113 @@
 namespace MatrixSpace {
-using VectorSpace;
+    using VectorSpace;
 
-public class Matrix {
-    double[,] data;
-    int rows, cols;
+    public class Matrix {
+        double[,] data;
+        int rows, cols;
 
-    public Matrix(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.data = new double[rows, cols];
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public int getColumns() {
-        return cols;
-    }
-
-    public string getOrder() {
-        return rows + "x" + cols;
-    }
-
-    public void set(int row, int col, double value) {
-        if (row > rows || row < 0) {
-            throw new System.Exception("Row out of bounds!");
-        } else if (col > cols || col < 0) {
-            throw new System.Exception("Column out of bounds!");
+        public Matrix(int rows, int cols) {
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[rows, cols];
         }
 
-        data[row, col] = value;
-    }
-
-    public double get(int row, int col) {
-        if (row > rows || row < 0) {
-            throw new System.Exception("Row out of bounds!");
-        } else if (col > cols || col < 0) {
-            throw new System.Exception("Column out of bounds!");
+        public int getRows() {
+            return rows;
         }
 
-        return data[row, col];
-    }
+        public int getColumns() {
+            return cols;
+        }
 
-    public Matrix add(Matrix other) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                data[i, j] += other.get(i, j);
+        public string getOrder() {
+            return rows + "x" + cols;
+        }
+
+        public void set(int row, int col, double value) {
+            if (row > rows || row < 0) {
+                throw new System.Exception("Row out of bounds!");
+            } else if (col > cols || col < 0) {
+                throw new System.Exception("Column out of bounds!");
             }
+
+            data[row, col] = value;
         }
 
-        return this;
-    }
-
-    public Matrix subtract(Matrix other) {
-        other.scale(-1);
-        this.add(other);
-        other.scale(-1);
-        return this;
-    }
-
-    public Matrix scale(double value) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                data[i, j] *= value;
+        public double get(int row, int col) {
+            if (row > rows || row < 0) {
+                throw new System.Exception("Row out of bounds!");
+            } else if (col > cols || col < 0) {
+                throw new System.Exception("Column out of bounds!");
             }
+
+            return data[row, col];
         }
 
-        return this;
-    }
-
-    public Matrix multiply(Matrix other) {
-        if (this.cols != other.rows) {
-            throw new System.Exception("Columns of the calling matrix must match rows of the other matrix!");
-        }
-
-        Matrix m = new Matrix(this.rows, other.cols);
-        for (int i = 0; i < m.rows; i++) {
-            Vector row = this.getRowVector(i);
-
-            for (int j = 0; j < m.cols; j++) {
-                Vector col = other.getColumnVector(j);
-
-                m.set(i, j, row.dot(col));
+        public Matrix add(Matrix other) {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < rows; j++) {
+                    data[i, j] += other.get(i, j);
+                }
             }
+
+            return this;
         }
 
-        return m;
-    }
-
-    public Vector getRowVector(int row) {
-        Vector v = new Vector(cols);
-
-        for (int i = 0; i < cols; i++) {
-            v.set(i, data[row, i]);
+        public Matrix subtract(Matrix other) {
+            other.scale(-1);
+            this.add(other);
+            other.scale(-1);
+            return this;
         }
 
-        return v;
-    }
+        public Matrix scale(double value) {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < rows; j++) {
+                    data[i, j] *= value;
+                }
+            }
 
-    public Vector getColumnVector(int col) {
-        Vector v = new Vector(rows);
-
-        for (int i = 0; i < rows; i++) {
-            v.set(i, data[i, col]);
+            return this;
         }
 
-        return v;
+        public Matrix multiply(Matrix other) {
+            if (this.cols != other.rows) {
+                throw new System.Exception("Columns of the calling matrix must match rows of the other matrix!");
+            }
+
+            Matrix m = new Matrix(this.rows, other.cols);
+            for (int i = 0; i < m.rows; i++) {
+                Vector row = this.getRowVector(i);
+
+                for (int j = 0; j < m.cols; j++) {
+                    Vector col = other.getColumnVector(j);
+
+                    m.set(i, j, row.dot(col));
+                }
+            }
+
+            return m;
+        }
+
+        public Vector getRowVector(int row) {
+            Vector v = new Vector(cols);
+
+            for (int i = 0; i < cols; i++) {
+                v.set(i, data[row, i]);
+            }
+
+            return v;
+        }
+
+        public Vector getColumnVector(int col) {
+            Vector v = new Vector(rows);
+
+            for (int i = 0; i < rows; i++) {
+                v.set(i, data[i, col]);
+            }
+
+            return v;
+        }
     }
-}
+    
 }
